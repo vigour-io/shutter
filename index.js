d7a91b34e611f917ff2f77731b68623eb23d43fd
Shawn Freyssonnet-Inder <shawn@vigour.io>
2014-08-10 20:29:28 +0200

    A little more modularity unclutters index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var express = require('express')
-	, http = require('http')
 	, bodyParser = require('body-parser')
-	, fs = require('fs')
-
 
 	Cloud = require('vigour-js/browser/network/cloud')
     .inject(require('vigour-js/browser/network/cloud/datacloud'))
   , Data = require('vigour-js/data')
 
+  , spriteMaker = require('./spriteMaker')
+
 	, config = require('./config')
-	, imgManip = require('./imgManip')
-	, cleanup = require('./cleanup')
 
 	, cloud = new Cloud('ws://' + config.cloudHost + ':' + config.cloudPort)
 	, data = new Data(cloud.data.get(config.mtvCloudDataFieldName))
@@ -62,14 +59,13 @@ app.use(bodyParser.urlencoded({
 app.get('/sprite/:country/:lang/shows/:width/:height'
 	, function (req, res, next) {
 		var p = req.params
-		requestSprite(req
-			, res
-			, next
-			, [p.country
-				, p.lang
-				, 'shows'
-			])
-	})
+		req.pathToSpriteData = [p.country
+			, p.lang
+			, 'shows'
+		]
+		next()
+	}
+	, spriteMaker.requestSprite)
 
 // app.get('/sprite/:country/:lang/channels/:width/:height'
 // 	, function (req, res, next) {
@@ -86,18 +82,17 @@ app.get('/sprite/:country/:lang/shows/:width/:height'
 app.get('/sprite/:country/:lang/episodes/:showId/:seasonId/:width/:height'
 	, function (req, res, next) {
 		var p = req.params
-		requestSprite(req
-			, res
-			, next
-			, [p.country
-				, p.lang
-				, 'shows'
-				, p.showId
-				, 'seasons'
-				, p.seasonId
-				, 'episodes'
-			])
-	})
+		req.pathToSpriteData = [p.country
+			, p.lang
+			, 'shows'
+			, p.showId
+			, 'seasons'
+			, p.seasonId
+			, 'episodes'
+		]
+		next()
+	}
+	, spriteMaker.requestSprite)
 
 app.get('*'
 	, function (req, res, next) {
@@ -110,133 +105,3 @@ data.addListener(function listen () {
 	console.log('Listening on port ', config.port);
 	this.removeListener(listen)
 })
-
-function requestSprite (req, res, next, path) {
-	var items = dive(data.raw, path)
-	if (items) {
-		getSprite(req, res, next, items)
-	} else {
-		res.status(400).end(config.invalidRequestMessage)
-	}
-}
-
-function dive (obj, path) {
-	var r = obj
-		, l = path.length
-		, i
-	for (i = 0; i < l; i += 1) {
-		r = r[path[i]]
-		if (!r) {
-			console.log(path[i] + ' not found')
-			return false
-		}
-	}
-	return r
-}
-
-function getSprite (req, res, next, items) {
-	var tmpDir = config.tmpDir + '/' + Math.random().toString().slice(1)
-		, desiredDimensions = {
-			width: req.params.width
-			, height: req.params.height
-		}
-	fs.mkdir(tmpDir, function (err) {
-		var ids = []
-			, l
-			, i
-			, nbLeft
-			, id
-			, url
-			, path
-			, paths = []
-			, errMessage
-		if (err) {
-			errMessage = "Error creating temp directory " + tmpDir + ": "
-			console.log(errMessage, err)
-			res.status(500).end(errMessage + err)
-		} else {
-			for (item in items) {
-				ids[items[item].number - 1] = items[item].img
-			}
-			nbLeft = l = ids.length
-			if (l === 0) {
-				res.status(400).end(config.invalidRequestMessage)
-			} else {
-				for (i = 0; i < l; i += 1) {
-					if (ids[i]) {
-						id = ids[i]
-						url = urlFromId(id)
-						path = tmpDir + '/' + ids[i]
-					} else {
-						url = false
-						path = 'images/mtv_logo_placeholder.png'
-					}
-					paths.push(path)
-					if (url) {
-						download(url, path, function (err) {
-							if (err) {
-								errMessage = "Error downloading " + url + ": "
-								console.log(errMessage, err)
-								res.status(500).end(errMessage + err)
-								cleanup(tmpDir)
-							} else {
-								nbLeft -= 1
-								getSpriteIfReady(nbLeft
-									, paths
-									, desiredDimensions
-									, tmpDir
-									, res)
-							}
-						})
-					} else {
-						nbLeft -= 1
-						getSpriteIfReady(nbLeft
-							, paths
-							, desiredDimensions
-							, tmpDir
-							, res)
-					}
-				}
-			}
-		}
-	})
-}
-
-function getSpriteIfReady (nbLeft, paths, desiredDimensions, tmpDir, res) {
-	var spritePath
-	if (nbLeft === 0) {
-		spritePath = config.spriteName + '.' + config.spriteFormat
-		imgManip.sprite(paths
-			, desiredDimensions
-			, spritePath
-			, function (err) {
-				var errMessage
-				if (err) {
-					errMessage = "Error creating sprite: "
-					console.log(errMessage, err)
-					res.status(500).end(errMessage + err)
-					cleanup(tmpDir)
-				} else {
-					res.sendfile(spritePath)
-					cleanup(tmpDir)
-				}
-			})
-	}
-}
-
-function urlFromId (id) {
-	return 'http://images.mtvnn.com/' + id + '/306x172'
-}
-
-function download (url, dest, cb) {
-	var file = fs.createWriteStream(dest)
-		, request = http.get(url, function (response) {
-			response.pipe(file)
-			file.on('finish', function () {
-				file.close(cb)
-			})
-		}).on('error', function (err) {
-			fs.unlink(dest)
-			cb(err.message)
-		})
-}
--- spriteMaker.js
+++ spriteMaker.js
@@ -1,8 +1,139 @@
-var gm = require('gm')
-	, config = require('./config')
-	, cleanup = require('./cleanup')
-	, imgManip = require('./imgManip')
+var fs = require('fs')
+	, http = require('http')
 
-module.exports = exports = function (dir, images, dimensions, cb) {
-	createSprite(dir, images, dimensions, '.resized.' + config.spriteFormat, 'sprite', cb)
+	, imgManip = require('./imgManip')
+	, cleanup = require('./cleanup')
+
+	, config = require('./config')
+
+module.exports = exports = {
+	requestSprite: function (req, res, next) {
+		var items = dive(data.raw, req.pathToSpriteData)
+		if (items) {
+			getSprite(req, res, next, items)
+		} else {
+			res.status(400).end(config.invalidRequestMessage)
+		}
+	}
+}
+
+function getSprite (req, res, next, items) {
+	var tmpDir = config.tmpDir + '/' + Math.random().toString().slice(1)
+		, desiredDimensions = {
+			width: req.params.width
+			, height: req.params.height
+		}
+	fs.mkdir(tmpDir, function (err) {
+		var ids = []
+			, l
+			, i
+			, nbLeft
+			, id
+			, url
+			, path
+			, paths = []
+			, errMessage
+		if (err) {
+			errMessage = "Error creating temp directory " + tmpDir + ": "
+			console.log(errMessage, err)
+			res.status(500).end(errMessage + err)
+		} else {
+			for (item in items) {
+				ids[items[item].number - 1] = items[item].img
+			}
+			nbLeft = l = ids.length
+			if (l === 0) {
+				res.status(400).end(config.invalidRequestMessage)
+			} else {
+				for (i = 0; i < l; i += 1) {
+					if (ids[i]) {
+						id = ids[i]
+						url = urlFromId(id)
+						path = tmpDir + '/' + ids[i]
+					} else {
+						url = false
+						path = 'images/mtv_logo_placeholder.png'
+					}
+					paths.push(path)
+					if (url) {
+						download(url, path, function (err) {
+							if (err) {
+								errMessage = "Error downloading " + url + ": "
+								console.log(errMessage, err)
+								res.status(500).end(errMessage + err)
+								cleanup(tmpDir)
+							} else {
+								nbLeft -= 1
+								getSpriteIfReady(nbLeft
+									, paths
+									, desiredDimensions
+									, tmpDir
+									, res)
+							}
+						})
+					} else {
+						nbLeft -= 1
+						getSpriteIfReady(nbLeft
+							, paths
+							, desiredDimensions
+							, tmpDir
+							, res)
+					}
+				}
+			}
+		}
+	})
+}
+
+function getSpriteIfReady (nbLeft, paths, desiredDimensions, tmpDir, res) {
+	var spritePath
+	if (nbLeft === 0) {
+		spritePath = config.spriteName + '.' + config.spriteFormat
+		imgManip.sprite(paths
+			, desiredDimensions
+			, spritePath
+			, function (err) {
+				var errMessage
+				if (err) {
+					errMessage = "Error creating sprite: "
+					console.log(errMessage, err)
+					res.status(500).end(errMessage + err)
+					cleanup(tmpDir)
+				} else {
+					res.sendfile(spritePath)
+					cleanup(tmpDir)
+				}
+			})
+	}
+}
+
+function urlFromId (id) {
+	return 'http://images.mtvnn.com/' + id + '/306x172'
+}
+
+function download (url, dest, cb) {
+	var file = fs.createWriteStream(dest)
+		, request = http.get(url, function (response) {
+			response.pipe(file)
+			file.on('finish', function () {
+				file.close(cb)
+			})
+		}).on('error', function (err) {
+			fs.unlink(dest)
+			cb(err.message)
+		})
+}
+
+function dive (obj, path) {
+	var r = obj
+		, l = path.length
+		, i
+	for (i = 0; i < l; i += 1) {
+		r = r[path[i]]
+		if (!r) {
+			console.log(path[i] + ' not found')
+			return false
+		}
+	}
+	return r
 }
