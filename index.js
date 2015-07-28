b108f69a8786654b05bde555c06a6537e2fc3475
nikoloza <nika.tomadze@gmail.com>
2015-07-28 11:54:13 +0200

    updated with super working snapshot

--- index.js
+++ index.js
@@ -440,8 +440,8 @@ function prepare(req, res, next) {
     .then(function() {
       req.tmpDir = config.tmpDir + '/' + Math.random().toString().slice(1)
 
-      log.info('creating temp directory'.cyan)
       fs.mkdir(req.tmpDir, function(err) {
+
         if (err) {
           err.detail = 'fs.mkdir error'
           err.path = req.tmpDir
