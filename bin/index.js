56b0a368eef327afcb8ead06a478b2310432630a
Shawn Inder <shawn@vigour.io>
2015-08-04 17:20:48 +0200

    Don't use exports in bin/index.js

--- bin/index.js
+++ bin/index.js
@@ -3,4 +3,4 @@ var pliant = require('pliant')
 var start = require('../lib/launcher')
 var config = require('../lib/config')
 
-module.exports = exports = pliant.bin(start, config)
+pliant.bin(start, config)
