52fcdab7b94227a1b37f380b8e65cbba57ac99a9
Shawn Inder <shawn@vigour.io>
2015-08-19 14:28:41 +0200

    Send 404 status when requested original image is not found (instead of 500) + test

---
+++ test/node/errors.js
@@ -0,0 +1,84 @@
+var path = require('path')
+var http = require('http')
+var imgServer = require('../../')
+var imgHandle
+var express = require('express')
+var server = express()
+var serverHandle
+var serverPort = 8001
+server.use(function (req, res, next) {
+	res.status(404).end()
+})
+
+describe("Errors", function () {
+	before(function (done) {
+		var s = false
+		var i = false
+		this.timeout(5000)
+		serverHandle = server.listen(serverPort, function () {
+			s = true
+			finish()
+		})
+		imgServer({ maxTries: 2 })
+			.then(function (_imgHandle) {
+				imgHandle = _imgHandle
+				i = true
+				finish()
+			})
+		function finish () {
+			if (s && i) {
+				done()
+			}
+		}
+	})
+
+	it("should send a 404 response when requested origin image is not found", function (done) {
+		var req = http.request({ path: "/image/400/600?url=http://localhost:" + serverPort
+			, port: 8000
+			}
+		, function (res) {
+			var total = ""
+			res.on('error', function (err) {
+				console.error("res error", err)
+				expect(err).not.to.exist
+				done()
+			})
+			if (res.statusCode !== 404) {
+				res.on('data', function (chunk) {
+					console.log("CHUNK", chunk.toString())
+				})
+				res.on('end', function () {
+					expect(res.statusCode).to.equal(404)
+					done()
+				})
+			} else {
+				expect(res.statusCode).to.equal(404)
+				done()
+			}
+		})
+		req.on('error', function (err) {
+			console.error("req error", err)
+			expect(err).not.to.exist
+			done()
+		})
+		req.end()
+	})
+
+	after(function (done) {
+		var s = false
+		var i = false
+		serverHandle.close(function () {
+			s = true
+			finish()
+		})
+		imgHandle.close(function () {
+			i = true
+			finish()
+		})
+		function finish () {
+			if (s && i) {
+				done()
+			}
+		}
+	})
+})
