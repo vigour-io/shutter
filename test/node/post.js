e64c3c5c79558c19b81763221409756f5a38a765
Shawn Inder <shawn@vigour.io>
2015-08-21 22:33:07 +0200

    Save post images at different paths to avoid collisions...

--- test/node/post.js
+++ test/node/post.js
@@ -9,87 +9,87 @@ var hash = require('vigour-js/util/hash.js')
 var handle
 
 describe("POST /image/:width/:height", function () {
-	var base = "/image/600/400?"
-	// var base = "http://shawn.vigour.io:8040/image/600/400?"
-	var effects =
-		[ ""
-		, "&effect=smartResize"
-		, "&effect=composite&overlay=overlay"
-		, "&effect=blur&radius=0&sigma=3"
-		, "&effect=tMask&mask=avatarMask"
-		, "&effect=mask&mask=logoMask&fillColor=EE255C"
-		, "&effect=overlay&overlay=overlay"
-		, "&effect=tMask&mask=logoMask"
-		, "&effect=overlayBlur&overlay=overlay&radius=0&sigma=3"
-		]
+  var base = "/image/600/400?cache=false"
+  // var base = "http://shawn.vigour.io:8040/image/600/400?"
+  var effects =
+    [ ""
+    , "&effect=smartResize"
+    , "&effect=composite&overlay=overlay"
+    , "&effect=blur&radius=0&sigma=3"
+    , "&effect=tMask&mask=avatarMask"
+    , "&effect=mask&mask=logoMask&fillColor=EE255C"
+    , "&effect=overlay&overlay=overlay"
+    , "&effect=tMask&mask=logoMask"
+    , "&effect=overlayBlur&overlay=overlay&radius=0&sigma=3"
+    ]
 
-	before(function (done) {
-		this.timeout(5000)
-		imgServer()
-			.then(function (_handle) {
-				handle = _handle
-				done()
-			})
-	})
+  before(function (done) {
+    this.timeout(5000)
+    imgServer()
+      .then(function (_handle) {
+        handle = _handle
+        done()
+      })
+  })
 
-	effects.map(function (effect) {
-		var fullPath = base + effect
-		it(effect, attempt(fullPath, effect))
-	})
-	after(function (done) {
-		handle.close(function () {
-			done()
-		})
-	})
+  effects.map(function (effect) {
+    var fullPath = base + effect
+    it(effect, attempt(fullPath, effect))
+  })
+  after(function (done) {
+    handle.close(function () {
+      done()
+    })
+  })
 })
 
 function attempt (fullPath, effect) {
-	return function (done) {
-		stat(sampleImage)
-			.then(function (stats) {
-				var rs = fs.createReadStream(sampleImage)
-				var req = http.request(
-					{ path: fullPath
-					, port: 8000
-					, method: "POST"
-					, headers:
-						{ "Content-Length": stats.size
-						, "Content-Type": "image/jpeg"
-						}
-					}
-				, function (res) {
-					res.on('error', function (err) {
-						console.error("err", err, err.stack)
-						expect(err).not.to.exist
-					})
-					if (res.statusCode !== 200) {
-						res.on('data', function (chunk) {
-							console.log("CHUNK", chunk.toString())
-						})
-						res.on('end', function () {
-							expect(true).to.equal(false)
-						})
-					} else {
-						expect(res.statusCode).to.equal(200)
-						var out = path.join(__dirname, 'out', effect || "noEffect")
-						var ws = fs.createWriteStream(out)
-						res.pipe(ws)
-						res.on('error', function (err) {
-							console.error("NOOOO", err)
-							expect(true).to.equal(false)
-						})
-						res.on('end', function () {
-							expect(true).to.equal(true)
-							done()	
-						})
-					}
-				})
-				req.on('error', function (err) {
-					expect(err).not.to.exist
-				})
-				rs.pipe(req).on('error', function (err) {
-					expect(err).not.to.exist
-				})
-			})
-	}
+  return function (done) {
+    stat(sampleImage)
+      .then(function (stats) {
+        var rs = fs.createReadStream(sampleImage)
+        var req = http.request(
+          { path: fullPath
+          , port: 8000
+          , method: "POST"
+          , headers:
+            { "Content-Length": stats.size
+            , "Content-Type": "image/jpeg"
+            }
+          }
+        , function (res) {
+          res.on('error', function (err) {
+            console.error("err", err, err.stack)
+            expect(err).not.to.exist
+          })
+          if (res.statusCode !== 200) {
+            res.on('data', function (chunk) {
+              console.log("CHUNK", chunk.toString())
+            })
+            res.on('end', function () {
+              expect(true).to.equal(false)
+            })
+          } else {
+            expect(res.statusCode).to.equal(200)
+            var out = path.join(__dirname, 'out', effect || "noEffect")
+            var ws = fs.createWriteStream(out)
+            res.pipe(ws)
+            res.on('error', function (err) {
+              console.error("NOOOO", err)
+              expect(true).to.equal(false)
+            })
+            res.on('end', function () {
+              expect(true).to.equal(true)
+              done()
+            })
+          }
+        })
+        req.on('error', function (err) {
+          expect(err).not.to.exist
+        })
+        rs.pipe(req).on('error', function (err) {
+          expect(err).not.to.exist
+        })
+      })
+  }
 }
