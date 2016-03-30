ba7d5b5919961224b5775beb503f9b406a8fe8d2
Boite Witte <boite@vigour.io>
2016-03-30 18:09:55 +0200

    init config

---
+++ config/env/develop.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  logger: {
+    name: 'vigour-gwf-develop'
+  }
+}
---
+++ config/schema.js
@@ -0,0 +1,114 @@
+'use strict'
+module.exports = {
+  debug: {
+    doc: 'Application in debug mode',
+    format: Boolean,
+    default: false,
+    env: 'DEBUG',
+    arg: 'debug'
+  },
+  env: {
+    doc: 'The applicaton environment.',
+    format: ['develop', 'test', 'staging', 'production'],
+    default: 'develop',
+    env: 'NODE_ENV',
+    arg: 'node-env'
+  },
+  maxCols: {
+    doc: '',
+    format: Number,
+    default: 10
+  },
+  maxTries: {
+    doc: '',
+    format: Number,
+    default: 6
+  },
+  invalidRequestMessage: {
+    doc: 'Message to display when a invalid request is made',
+    format: String,
+    default: 'Invalid request. Make sure you are respecting the api (https://github.com/vigour-io/shutter/blob/master/README.md#user-content-api) and that the requested data exists.'
+  },
+  images: {
+    sizes: {
+      maxWidth: {
+        doc: 'The Maximum width of the images',
+        format: Number,
+        default: 10000
+      },
+      maxHeight: {
+        doc: 'The Maximum height of the images',
+        format: Number,
+        default: 10000
+      }
+    }
+  },
+  minFreeSpace: {
+    doc: 'The minimum amount of free space needed',
+    format: Number,
+    default: 0.1
+  },
+  convertPath: {
+    doc: 'Path to Image Magick\'s `convert` executable',
+    format: String,
+    default: '/usr/bin/convert'
+  },
+  identifyPath: {
+    doc: 'Path to Image Magick\'s `identify` executable',
+    format: String,
+    default: '/usr/bin/indentify'
+  },
+  clean: {
+    doc: 'Removes all downloaded originals, produced images and temporary folders and files, then exits',
+    format: Boolean,
+    default: false
+  },
+  remote: {
+    host: {
+      doc: 'URL for the remote image server to use for image manipulations',
+      format: String,
+      default: 'img.vigour.io'
+    },
+    port: {
+      doc: 'Port on which to reach the remote image server',
+      format: Number,
+      default: 80
+    }
+  },
+  fallbacks: {
+    doc: 'JSON string describing fallback images. see README.md'
+  },
+  retryOn404: {
+    doc: 'Should we try to retrieve the image again on 404',
+    format: Boolean,
+    default: false
+  },
+  clients: {
+    aws: {
+      accessKeyId: {
+        doc: 'Access Key ID for authenticating requests against AWS',
+        format: String,
+        default: null,
+        env: 'SHUTTER_AWS_ACCESS_KEY_ID'
+      },
+      secretAccesKey: {
+        doc: 'Secret Access Key for authenticating requests against AWS',
+        format: String,
+        default: null,
+        env: 'SHUTTER_AWS_SECRET_ACCESS_KEY'
+      },
+      cloudfront: {
+        apiVersion: {
+          doc: 'aws-sdk api version to use when invalidating cloudfront assets',
+          format: String,
+          default: '2016-01-28'
+        },
+        distributionId: {
+          doc: 'ID of the distribution on CloudFront on which to perform invalidations',
+          format: String,
+          default: 'SHUTTER_CLOUDFRONT_DISTRIBUTION_ID'
+        }
+      }
+    }
+  }
+}
